7327a08907598ca02de8250304ae415ea172cdb2
EthanKaiFal <ethan.kai.falconer@gmail.com>
2025-03-17 00:38:21 -0700

    completed the bento box area
    small adjustments needed

--- 3dport/src/sections/About.jsx
+++ 3dport/src/sections/About.jsx
@@ -1,11 +1,20 @@
 import Globe from 'react-globe.gl'
 import { Button } from '../components/Button';
+import { useState } from 'react';
 
 const About = () => {
+    const [isCopied, setCopied] = useState(false);
+    const handleCopy=()=> {
+        navigator.clipboard.writeText('ethan.kai.falconer@gmail.com');
+        setCopied(true);
+        setTimeout(()=>{
+            setHasCopied(false);
+        }, 2000);
+    }
     return(
         <section className="c-space" style={{marginTop:"20px", marginBottom:"20px"}}>
             About
-            <div className="grid-setup xl-grid md-grid" style={{display:"grid"}}>
+            <div className="xl-grid md-grid bento-setup">
                 <div className="grid-item1">
                     <div className="grid-container">
                         <img src="/assets/grid1.png" alt="grid1" className="grid-img"/>
@@ -15,13 +24,14 @@ const About = () => {
                         </div>
                     </div>
                 </div>
-                <div className="grid-item1">
+                <div className="grid-item3">
                     <div className="grid-container">
-                    <img src="/assets/grid2.png" alt="grid2" className="grid-img"/>
-                        <div>
-                            <p className="grid-headtext">Tech Stack</p>
-                            <p className="grid-subtext">I specialize in, JavaScript/TypeScript with a focus on React and Next.js ecosystems with additional deployment support provided through AWS</p>
-                        </div>
+                        <img src="/assets/grid3.png" alt='grid-3' className='grid-img'/>
+                            <div style={{color:"white"}}>
+                                <p className="grid-headtext">My passion for coding</p>
+                                <p clasName="grid-subtext">I enjoy the act of creating projects that I can get great convenient use out of. Coding has been my chosen method of satisfying this desire and I see it as way more than a profession but instead a core method to find satisfaction in life.  </p>
+                            </div>
+
                     </div>
                 </div>
                 <div className="grid-item2">
@@ -45,6 +55,28 @@ const About = () => {
                         <p className="grid-headtext">Currently looking to work anywhere in the US.</p>
                             <p className="grid-subtext">I'm currently located in the Los Angeles area but very open to relocation anywhere in the country.</p>
                             <Button name="Contact Me" isBeam/>
+                    </div>
+                </div>
+                <div className="grid-item5">
+                    <div className="grid-container">
+                    <img src="/assets/grid2.png" alt="grid2" className="grid-img"/>
+                        <div>
+                            <p className="grid-headtext">Tech Stack</p>
+                            <p className="grid-subtext">I specialize in, JavaScript/TypeScript with a focus on React and Next.js ecosystems with additional deployment support provided through AWS</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="grid-item4">
+                    <div className="grid-container">
+                        <img src="assets/grid4.png" alt="grid4" className="grid-img2"/>
+                            <div className="custom-space-y">
+                                <p className="grid-subtext contact-me">Contact Me</p>
+                                <div className = "copy-container" onClick={handleCopy}>
+                                    <img src={isCopied? 'assets/tick.svg': 'assets/copy.svg'} alt="copy"></img>
+                                    <p style={{color:"white"}}>ethan.kai.falconer@gmail.com</p>
+                                </div>
+                            </div>
                     </div>
                 </div>
             </div>
